be1489e8bab0c43035408eb9a30a17e608da55cc
ikneg <0930019@gmail.com>
2023-06-05 13:59:10 +0900

    display

---
+++ display.rs
@@ -0,0 +1,51 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "min: {}, max: {}", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+fn main() {
+    let minmax = MinMax(-300, 300);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let complex = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+
+    println!("Compare Complext structures:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+}
